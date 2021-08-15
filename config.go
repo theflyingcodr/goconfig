3f75eec9dd41586d029dedd5e8a0e3b3d5e660bd
Mark Smith <mark@theflyingcodr.dev>
2021-08-15 22:04:36 +0100

    adding some db config stoof

---
+++ config.go
@@ -0,0 +1,111 @@
+package config
+
+import (
+	"fmt"
+	"regexp"
+	"time"
+
+	validator "github.com/theflyingcodr/govalidator"
+)
+
+const (
+	EnvServerPort = "server.port"
+	EnvServerHost = "server.host"
+
+	EnvEnvironment = "env.environment"
+	EnvMainNet     = "env.mainnet"
+	EnvRegion      = "env.region"
+	EnvVersion     = "env.version"
+	EnvCommit      = "env.commit"
+	EnvBuildDate   = "env.builddate"
+	EnvLogLevel    = "log.level"
+
+	EnvDb        = "db.type"
+	EnvDbSchema  = "db.schema.path"
+	EnvDbDsn     = "db.dsn"
+	EnvDbMigrate = "db.migrate"
+
+	LogDebug = "debug"
+	LogInfo  = "info"
+	LogError = "error"
+	LogWarn  = "warn"
+)
+
+// Config returns strongly typed config values.
+type Config struct {
+	defaultsFn func(c Config) error
+	Logging    *Logging
+	Server     *Server
+	Deployment *Deployment
+	Db         *Db
+}
+
+// Validate will check config values are valid and return a list of failures
+// if any have been found.
+func (c *Config) Validate() error {
+	vl := validator.New()
+	if c.Db != nil {
+		vl = vl.Validate("db.type", validator.MatchString(string(c.Db.Type), reDbType))
+	}
+	return vl.Err()
+}
+
+// Deployment contains information relating to the current
+// deployed instance.
+type Deployment struct {
+	Environment string
+	AppName     string
+	Region      string
+	Version     string
+	Commit      string
+	BuildDate   time.Time
+	MainNet     bool
+}
+
+// IsDev determines if this app is running on a dev environment.
+func (d *Deployment) IsDev() bool {
+	return d.Environment == "dev"
+}
+
+func (d *Deployment) String() string {
+	return fmt.Sprintf("Environment: %s \n AppName: %s\n Region: %s\n Version: %s\n Commit:%s\n BuildDate: %s\n",
+		d.Environment, d.AppName, d.Region, d.Version, d.Commit, d.BuildDate)
+}
+
+type Logging struct {
+	Level string
+}
+
+// Server contains all settings required to run a web server.
+type Server struct {
+	Port     string
+	Hostname string
+}
+
+// Db contains database information.
+type Db struct {
+	Type       string
+	SchemaPath string
+	Dsn        string
+	Migrate    bool
+}
+
+var reDbType = regexp.MustCompile(`sqlite|mysql|postgres`)
+
+// DbType is used to restrict the dbs we can support.
+type DbType string
+
+// Supported database types.
+const (
+	DBSqlite   DbType = "sqlite"
+	DBMySql    DbType = "mysql"
+	DBPostgres DbType = "postgres"
+)
+
+// ConfigurationLoader will load configuration items
+// into a struct that contains a configuration.
+type ConfigurationLoader interface {
+	WithServer() *Config
+	WithDb() *Config
+	WithDeployment(app string) *Config
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/theflyingcodr/config
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/golang-migrate/migrate/v4 v4.14.1
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/lib/pq v1.8.0
+	github.com/libsv/bitcoin-hc v0.0.2
+	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.8.1
+	github.com/theflyingcodr/govalidator v0.0.2
+)
